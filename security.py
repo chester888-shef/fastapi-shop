fe41778c74719252948a5727bf7594ef83a691ff
chester888 <maksimfedun0@gmail.com>
2026-09-24 18:03:15 +0300

    Update security.py

--- security.py
+++ security.py
@@ -5,7 +5,7 @@ from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 from models import User
 
-SECRET_KEY = "arsen_loh"
+SECRET_KEY = "arseт"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 pwd_context = CryptContext(schemes =["bcrypt"], deprecated = "auto")
 
